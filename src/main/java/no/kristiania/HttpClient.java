c2722514787181dc006e6008d7497b6bd08ec571
Sebastian <Sebastian.eilertsenn@gmail.com>
2020-09-23 15:57:40 +0200

    Red test in HttpClientTest

--- src/main/java/no/kristiania/HttpClient.java
+++ src/main/java/no/kristiania/HttpClient.java
@@ -1,7 +1,33 @@
 package no.kristiania;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
-    public static void main(String[] args) {
-        System.out.println("Client");
+
+    private int statusCode = 200;
+
+    public HttpClient(String hostName, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(hostName, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostName + "\r\n\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while((c = socket.getInputStream().read()) != -1) {
+            if (c != '\n') break;
+            System.out.print((char)c);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new HttpClient("urlecho.appspot.com", 80, "/echo?status=200&body=Hello%20world!");
+
+
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
