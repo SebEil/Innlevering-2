16547646054e6546013edae56db4455e9d6db439
SebEil <56046884+SebEil@users.noreply.github.com>
2020-09-23 14:53:47 +0200

    QueryString and all green test from earlier classes

--- src/main/java/no/kristiania/QueryString.java
+++ src/main/java/no/kristiania/QueryString.java
@@ -1,4 +1,22 @@
 package no.kristiania;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
+        private Map<String, String> parameters = new HashMap<>();
+
+        public QueryString(String queryString){
+            for (String parameter : queryString.split("&")) {
+                int equalPos = parameter.indexOf("=");
+                String value = parameter.substring(equalPos+1);
+                String parameterName = parameter.substring(0, equalPos);
+                parameters.put(parameterName, value);
+            }
+        }
+
+        public String getParameter(String name) {
+            return parameters.get(name);
+        }
 }
+
--- src/test/java/no/kristiania/QueryStringTest.java
+++ src/test/java/no/kristiania/QueryStringTest.java
@@ -1,4 +1,34 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class QueryStringTest {
+
+    @Test
+    void ShouldRetrieveQueryParameters(){
+        QueryString queryString = new QueryString("Status=200");
+        assertEquals("200", queryString.getParameter("Status"));
+    }
+
+    @Test
+    void ShouldRetrieveOtherQueryParameters(){
+        QueryString queryString = new QueryString("Status=400");
+        assertEquals("400", queryString.getParameter("Status"));
+    }
+
+    @Test
+    void ShouldRetrieveParameterByName(){
+        QueryString queryString = new QueryString("text=Hello");
+        assertEquals("Hello", queryString.getParameter("text"));
+        assertEquals(null, queryString.getParameter("status"));
+    }
+
+    @Test
+    void ShouldHandleMultipleParameters(){
+        QueryString queryString = new QueryString("text=Hello&status=200");
+        assertEquals("Hello", queryString.getParameter("text"));
+        assertEquals("200", queryString.getParameter("status"));
+    }
+
 }
