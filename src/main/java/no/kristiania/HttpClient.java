e09a73f9dacd19277779945c38adf5026503196f
Sebastian <Sebastian.eilertsenn@gmail.com>
2020-09-25 01:20:18 +0200

    Took away messages

--- src/main/java/no/kristiania/HttpClient.java
+++ src/main/java/no/kristiania/HttpClient.java
@@ -10,7 +10,6 @@ public class HttpClient {
     private String responseBody;
     private HttpMessage responseMessage;
 
-    //!!
     public HttpClient(String hostName, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(hostName, port);
 
--- src/main/java/no/kristiania/HttpServer.java
+++ src/main/java/no/kristiania/HttpServer.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HttpServer {
-//!!
+
     private static File documentRoot;
     private List<String> workerNames = new ArrayList<>();
 
